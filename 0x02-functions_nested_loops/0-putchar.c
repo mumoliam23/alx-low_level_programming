c39f1fd128afe136848083a9755c66f1219d3198
mumoliam23 <mumoliam23@gmail.com>
2023-09-12 23:07:42 +0300

    Created  a README.md file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - Entry
+ *
+ * Description: prints _putchar using putchar prototype
+ *
+ * Return: 0
+ */
+
+int main(void)
+
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+
+	_putchar('\n');
+
+	return (0);
+
+}
