2767ad6ef37994111230ccefc1ddc085fd2c6ab9
mumoliam23 <mumoliam23@gmail.com>
2023-09-13 00:14:39 +0300

    Changes made

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,24 +1,16 @@
 #include "main.h"
 
-/**
- * main - Entry
- *
- * Description: prints _putchar using putchar prototype
- *
- * Return: 0
- */
-
 int main(void)
-
 {
-	char str[] = "_putchar";
-	int ch;
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
 
-	for (ch = 0; ch < 8; ++ch)
-		_putchar(str[ch]);
-
-	_putchar('\n');
-
-	return (0);
-
+    return (0);
 }
+
