87176ec2c39452949196e47e062a288d92f1512e
mumoliam23 <mumoliam23@gmail.com>
2023-09-15 04:44:13 +0300

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - from 0 to 9.
+ *       use _putchar twice in your code
+ *
+ * Return: Always 0.
+*/
+
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+_putchar(i + 48);
+}
+
+_putchar('\n');
+}
