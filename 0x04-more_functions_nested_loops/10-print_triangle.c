e9ad73a9061494f12b703465cb25c2258572c1f8
mumoliam23 <mumoliam23@gmail.com>
2023-09-15 05:02:07 +0300

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle using _putchar
+ *
+ * @size: size of triangle
+ *
+ * Return: Always 0 (Success)
+*/
+
+void print_triangle(int size)
+{
+int x, y;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (x = 1 ; x <= size ; ++x)
+{
+for (y = 1 ; y <= size ; ++y)
+{
+if ((y + x) <= size)
+{
+_putchar(' ');
+}
+else
+{
+_putchar('#');
+}
+}
+_putchar('\n');
+}
+}
+}
