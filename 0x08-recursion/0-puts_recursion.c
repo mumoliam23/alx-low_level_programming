fe8bf3133ae451138412b09ce8965b77161722ad
mumoliam23 <mumoliam23@gmail.com>
2023-09-28 01:10:30 +0300

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string, one character at a time
+ * @s: pointer to the string to be printed
+ * Return: no return value
+ */
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s + 1);
+}
