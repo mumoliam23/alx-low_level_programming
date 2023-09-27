efc5dd6b85c52ad10983109437e3cf81e9239402
mumoliam23 <mumoliam23@gmail.com>
2023-09-27 04:03:10 +0300

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,27 +1,26 @@
 #include "main.h"
-
-#include "main.h"
-#include <stdio.h>
-
 /**
-  * main - check the code for ALX School students.
-  *
-  * Return: Always 0.
-  */
+ * print_chessboard - Print the chessboard
+ * @a: array of pieces
+ *
+ * Return: Nothing.
+*/
 
-int main(void)
+void print_chessboard(char (*a)[8])
 {
-char *_strpbrk(char *s, char *accept)
-char board[8][8] = {
-{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
-{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-{'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
-};
-print_chessboard(board);
-return (0);
+int i, j;
+
+i = 0;
+j = 0;
+while (i < 64)
+{
+if (i % 8 == 0 && i != 0)
+{
+j = i;
+_putchar('\n');
+}
+_putchar(a[i / 8][i - j]);
+i++;
+}
+_putchar('\n');
 }
