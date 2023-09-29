c73da94d68bea321cbd9bc2d0f78c3521e12bea5
mumoliam23 <mumoliam23@gmail.com>
2023-09-29 23:51:02 +0300

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point to the program
+ * @argc: Number of command line arguments
+ * @argv: Array of strings that represent command line arguments
+ * Return: Always 0
+ */
+int main(int argc, char **argv)
+{
+(void)argv;
+printf("%d\n", argc);
+return (0);
+}
