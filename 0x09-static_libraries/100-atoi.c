6153f93e40f7f087b1451631ff62684ed8f6a7c0
mumoliam23 <mumoliam23@gmail.com>
2023-09-30 02:51:42 +0300

    updates to the library

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string into an integer.
+ *
+ * @s: the string to use.
+ *
+ * Return: integer.
+ */
+
+int _atoi(char *s)
+{
+int sign = 1;
+int result = 0;
+int i = 0;
+while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n')
+{
+i++;
+}
+if (s[i] == '-')
+{
+sign = -1;
+i++;
+}
+else if (s[i] == '+')
+{
+i++;
+}
+while (s[i] >= '0' && s[i] <= '9')
+{
+result = result * 10 + (s[i] - '0');
+i++;
+}
+return (sign * result);
+}
---
+++ 0x09-static_libraries/5-strstr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strstr - locate a substring
+ * @haystack: the string to search
+ * @needle: the string to find
+ * Return: pointer to the beginning of the located substring
+ */
+char *_strstr(char *haystack, char *needle)
+{
+int index;
+if (*needle == 0)
+return (haystack);
+while (haystack)
+{
+index = 0;
+if (haystack[index] == needle[index])
+{
+do {
+if (needle[index + 1] == '\0')
+return (haystack);
+index++;
+}
+while (haystack[index] == needle[index])
+}
+haystack++;
+}
+return (0);
+}
