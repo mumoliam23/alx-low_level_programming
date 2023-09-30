be179ee6467223f56e43c13c92ad8df01470ead6
mumoliam23 <mumoliam23@gmail.com>
2023-09-30 03:38:06 +0300

    updated lib

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _strstr - locate a substring
  * @haystack: the string to search
@@ -8,9 +9,9 @@
 char *_strstr(char *haystack, char *needle)
 {
 int index;
-if (*needle == 0)
+if (*needle == '\0')
 return (haystack);
-while (haystack)
+while (*haystack)
 {
 index = 0;
 if (haystack[index] == needle[index])
@@ -19,8 +20,7 @@ do {
 if (needle[index + 1] == '\0')
 return (haystack);
 index++;
-}
-while (haystack[index] == needle[index]);
+} while (haystack[index] == needle[index]);
 }
 haystack++;
 }
