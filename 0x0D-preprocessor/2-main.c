063eb106809e3166d396f422ca4148c69d83bf66
mumoliam23 <mumoliam23@gmail.com>
2023-10-10 01:42:59 +0300

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - name of the file is printed
+ *
+ * Return: Always return success
+ *
+ */
+int main(void)
+{
+printf("%s\n", _FILE_);
+return (0);
+}
