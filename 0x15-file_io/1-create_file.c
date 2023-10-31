0fd913d33d850a569b6885351aae0c68d4b49865
mumoliam23 <mumoliam23@gmail.com>
2023-10-31 03:01:32 +0300

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,22 +9,25 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int fd;
-int nletters;
-ssize_t bytes_written;
+	int file, write_status, words = 0;
 
-if (!filename)
-	return (-1);
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (filename == NULL)
+		return (-1);
 
-if (fd == -1)
-	return (-1);
-if (!text_content)
-	text_content = "";
-for (nletters = 0; text_content[nletter]; nletters++)
-	;
-bytes_written = write(fd, text_content, nletters);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (file == -1)
+		return (-1);
 
-close(fd);
-return (1);
+	if (text_content)
+	{
+		while (text_content[words] != '\0')
+			words++;
+
+		write_status = write(file, text_content, words);
+		if (write_status == -1)
+			return (-1);
+	}
+
+	close(file);
+	return (1);
 }
