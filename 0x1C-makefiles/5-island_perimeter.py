8644180693523e5d99793f98cd177348e739bdbd
mumoliam23 <mumoliam23@gmail.com>
2024-02-22 04:23:09 +0300

    0x1C. C - Makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """Defines an island perimeter measuring function."""
 
-def island_perimeter(grid):
-    """Return the perimeter of an island.
 
-    Grid represents water by 0 and land by 1.
+def island_perimeter(grid):
+    """Return the perimiter of an island.
+
+    The grid represents water by 0 and land by 1.
 
     Args:
-        grid(lists): List of list of integers representing an island.
+        grid (list): A list of list of integers representing an island.
     Returns:
         The perimeter of the island defined in grid.
     """
